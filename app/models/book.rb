52545cd78a0caf05ec50fc1e3d5fb425929a510f
EC2 Default User <ec2-user@ip-172-31-15-205.ap-northeast-1.compute.internal>
2023-05-01 00:29:16 +0000

    correct

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,6 @@ class Book < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  validates :comment,  presence: true
 
   def favorited_by?(user)
       favorites.exists?(user_id: user.id)
