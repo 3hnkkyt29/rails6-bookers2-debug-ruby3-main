71661457a640c29a0f269bfec590314bea4f4e87
EC2 Default User <ec2-user@ip-172-31-15-205.ap-northeast-1.compute.internal>
2023-05-01 08:24:31 +0000

    correct view

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
+
   # フォローフォロワー機能
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
@@ -28,14 +28,14 @@ class User < ApplicationRecord
 
 # フォローフォロワー機能
   def follow(user_id)
-  relationships.create(followed_id: user_id)
+    relationships.create(followed_id: user_id)
   end
 
   def unfollow(user_id)
-  relationships.find_by(followed_id: user_id).destroy
+    relationships.find_by(followed_id: user_id).destroy
   end
 
   def following?(user)
-  followings.include?(user)
+    followings.include?(user)
   end
 end
